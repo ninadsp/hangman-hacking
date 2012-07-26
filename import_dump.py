8896c01bb8924d264e0db4fb8b6f56709c3eaa1d
ninadsp <ninadsp16289@gmail.com>
2012-07-26 22:56:48 +0530

    Improved dump_movies(), implemented dump_language()
    Added:
    
    Modified:
    	import_dump.py
    		dump_movies()
    			* improved reg ex and combined it into one
    			* removed commented out code
    		dump_language() - implemented, now works
    
    Removed:

--- import_dump.py
+++ import_dump.py
@@ -10,6 +10,8 @@ RATINGS_LIST = "ratings.list";
 LANGUAGE_LIST = "language.list";
 
 MOVIES_START = "===========";
+LANGUAGE_START = "=============";
+RATINGS_START= "";
 
 connection = Connection(dbconfig.host, dbconfig.port);
 db = connection[dbconfig.database];
@@ -19,7 +21,6 @@ collection.ensure_index([("title", pymongo.ASCENDING), ("year", pymongo.ASCENDIN
 
 
 def dump_movies():
-	# movies_fp = open(DATA_DIR + MOVIES_LIST, 'r');
 	movies_fp = codecs.open(DATA_DIR + MOVIES_LIST, 'r', encoding="cp1252");
 	movies_lines = movies_fp.readlines();
 	movies_fp.close();
@@ -33,28 +34,23 @@ def dump_movies():
 	i +=2 ;
 	movies_list = movies_lines[i:-1];
 
-	movies_lines = [];
-
 	print "Found " + str(len(movies_list)) + " movies";
 
 	movies = [];
 
-	reg_ex1 = re.compile("\(\d{4}\)");
-	reg_ex2 = re.compile("(\d{4})$");
+	reg_ex = re.compile("^\"?(.+)\"?\s*\((\d{4})\)(?:\t*)([?0-9]{4})$");
 
 	for movie in movies_list:
-		parts1 = reg_ex1.split(movie);
-		title = parts1[0].strip('"').lstrip('"');
-		parts2 = reg_ex2.split(movie);
-
-		# Handle lines where IMDb does not know the year of the title
-		try:
-			year = parts2[1];
-		except IndexError:
-			year = "0";
+		
+		match = reg_ex.search(movie);
+		if(match):
+			parts = match.groups();
+			title = parts[0].strip('" ');
+			year = parts[1];
+		else:
 			continue;
 
-		insert = {"ttle" : title, "year" : year};
+		insert = {"title" : title, "year" : year};
 		movies.append(insert);
 		if(len(movies) > 100):
 			collection.insert(movies);
@@ -64,15 +60,59 @@ def dump_movies():
 
 	print "Inserted " + str(collection.count()) + " unique entries into the movies table";
 
+	# Clean up before we leave
+	movies[:] = [];
+	movies_lines[:] = [];
+	movies_list[:] = [];
+
 
 def clean_movies():
 	collection.remove();
 
 def dump_language():
-	pass;
+	language_fp = codecs.open(DATA_DIR + LANGUAGE_LIST, 'r', encoding='cp1252');
+	language_lines = language_fp.readlines();
+	language_fp.close();
+
+	print "Read " + str(len(language_lines)) + " lines from file " + DATA_DIR + LANGUAGE_LIST;
+
+	for i in range(0, len(language_lines)):
+		i += 1;
+		if(LANGUAGE_START in language_lines[i]):
+			break;
+	i +=1;
+	language_list = language_lines[i:-1];
+
+	print "Found " + str(len(language_list)) + " language lines";
+	update_count = 0;
+
+	reg_ex = re.compile("^\"?(.+)\"?\s*\((\d{4})\)(?:\t*)([-a-zA-Z ]+)\s*(?:\(.*\))?$");
+
+	for lang in language_list:
+		match_obj = reg_ex.search(lang);
+		if(match_obj):
+			parts = match_obj.groups();
+			title = parts[0].strip('" ');
+			year = parts[1];
+			language = parts[2];
+
+		else:
+			continue;
+
+		collection.update( {"title" : title, "year" : year}, { "$set" : {"language" : language} } );
+		update_count += 1;
+
+	print "Updated " + str(update_count) + " entries of movies with language data";
+
+	language_list[:] = [];
+	language_lines[:] = [];
+
+
 
 def dump_ratings():
 	pass;
 
 dump_movies();
-# clean_movies();
+dump_language();
+#dump_ratings();
+#clean_movies();
